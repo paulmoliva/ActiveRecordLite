0187cbd734146d7c7ffbef515f667fb7c3980273
paulmoliva <paulmoliva@gmail.com>
2016-08-09 17:43:52 -0700

    finished activerecord lite

--- skeleton/lib/03_associatable.rb
+++ skeleton/lib/03_associatable.rb
@@ -28,11 +28,10 @@ class BelongsToOptions < AssocOptions
 end
 
 class HasManyOptions < AssocOptions
-  def initialize(name,  options = {})
-    # byebug
+  def initialize(name, classname, options = {})
     options.keys.each{|k| instance_variable_set("@#{k}", options[k])}
     @primary_key ||= :id
-    @foreign_key ||= (name.to_s.singularize.underscore + "_id").to_sym
+    @foreign_key ||= (classname.to_s.singularize.underscore + "_id").to_sym
     @class_name ||= name.to_s.camelize.singularize
   end
 end
@@ -41,25 +40,26 @@ module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
     options = BelongsToOptions.new(name, options)
+    assoc_options[name] = options if options.is_a?(BelongsToOptions)
     define_method(name) do
       foreign_key_value = send(options.foreign_key)
       target_model_class = options.model_class
-      target_model_class.where({options.primary_key => id}).first
+      target_model_class.where({options.primary_key => foreign_key_value}).first
     end
   end
 
-  def has_many(name, options = {})
-    options = HasManyOptions.new(name, options)
+  def has_many(name, options_params = {})
+    options = HasManyOptions.new(name, self.name, options_params)
     define_method(name) do
       primary_key_value = send(options.primary_key)
       target_model_class = options.model_class
-      # byebug
       target_model_class.where({options.foreign_key => primary_key_value})
     end
   end
 
   def assoc_options
     # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_options ||= {}
   end
 end
 
